a498b931c787355fc8f9cc5ba8903310c78e3981
fabienchapuis <fabien-chapuis@netc.fr>
2019-06-02 16:07:04 +0200

    ajout barre de recherche js + essai page erreur 404

--- MVC/view/assets/js/script.js
+++ MVC/view/assets/js/script.js
@@ -126,3 +126,184 @@ function showPopup(e) {
     //     .openOn(mymap);
     console.log("coucou");
 }
+
+
+let lati;
+let longi;
+let coords;
+let radius;
+let adressePostale;
+let adresse = document.getElementById('adresse'),
+    suggestions = document.getElementById('suggestions');
+
+
+
+$(function () {
+
+    // ------------------------------------Constante principales------------------------------------------
+    const apiAdresse = 'https://api-adresse.data.gouv.fr/search/?q=';
+    const apiAdressReverse = 'https://api-adresse.data.gouv.fr/reverse/';
+    const apiDate1 = 'https://opendata.doubs.fr/api/records/1.0/search/';
+    const apiDate2 = 'https://opendata.doubs.fr/api/records/1.0/search/';
+    //----------------------------autocompletion de l'adresse avec API adresse data gouv------------------------------------
+    (function () {
+
+        let selectedResult = -1, // Permet de savoir quel résultat est sélectionné : -1 signifie "aucune sélection"
+            previousRequest, // On stocke notre précédente requête dans cette variable
+            previousValue = adresse.value; // On fait de même avec la précédente valeur
+
+        function getResults(keywords) { // Effectue une requête et récupère les résultats
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', apiAdresse + encodeURIComponent(keywords));
+
+            xhr.addEventListener('readystatechange', function () {
+                if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                    let reponse = xhr.response;
+                    reponse = JSON.parse(reponse);
+                    reponse = reponse.features;
+                    let reponse0 = reponse[0].properties.label;
+                    let reponse1 = reponse[1].properties.label;
+                    let reponse2 = reponse[2].properties.label;
+                    let reponse3 = reponse[3].properties.label;
+                    let reponse4 = reponse[4].properties.label;
+                    let reponses = [reponse0, reponse1, reponse2, reponse3, reponse4];
+                    displayResults(reponses);
+
+                }
+            });
+            if (keywords.length >= 2) {
+                xhr.send(null);
+            }
+            return xhr;
+
+        }
+
+        function displayResults(reponses) { // Affiche les résultats d'une requête
+
+            suggestions.style.display = reponses.length ? 'block' : 'none'; // On cache le conteneur si on n'a pas de résultats
+
+            if (reponses.length) { // On ne modifie les résultats que si on en a obtenu
+
+
+                var responseLen = reponses.length;
+
+                suggestions.innerHTML = ''; // On vide les résultats
+
+                for (var i = 0, li; i < responseLen; i++) {
+
+                    li = suggestions.appendChild(document.createElement('li'));
+                    li.innerHTML = reponses[i];
+
+                    li.addEventListener('click', function (e) {
+                        chooseResult(e.target);
+                    });
+
+                }
+
+            }
+
+        }
+
+        function chooseResult(result) { // Choisi un des résultats d'une requête et gère tout ce qui y est attaché
+
+            adresse.value = previousValue = result.innerHTML; // On change le contenu du champ de recherche et on enregistre en tant que précédente valeur
+            suggestions.style.display = 'none'; // On cache les résultats
+            result.className = ''; // On supprime l'effet de focus
+            selectedResult = -1; // On remet la sélection à "zéro"
+            adresse.focus(); // Si le résultat a été choisi par le biais d'un clique alors le focus est perdu, donc on le réattribue
+
+        }
+
+        adresse.addEventListener('keyup', function (e) {
+
+            var lis = suggestions.getElementsByTagName('li');
+
+            if (e.keyCode == 38 && selectedResult > -1) { // Si la touche pressée est la flèche "haut"
+
+                lis[selectedResult--].className = '';
+
+                if (selectedResult > -1) { // Cette condition évite une modification de childNodes[-1], qui n'existe pas, bien entendu
+                    lis[selectedResult].className = 'result_focus';
+                }
+
+            } else if (e.keyCode == 40 && selectedResult < lis.length - 1) { // Si la touche pressée est la flèche "bas"
+
+                suggestions.style.display = 'block'; // On affiche les résultats
+
+                if (selectedResult > -1) { // Cette condition évite une modification de childNodes[-1], qui n'existe pas, bien entendu
+                    lis[selectedResult].className = '';
+                }
+
+                lis[++selectedResult].className = 'result_focus';
+
+            } else if (e.keyCode == 39 && selectedResult > -1) { // Si la touche pressée est "Entrée"
+
+                chooseResult(lis[selectedResult]);
+
+            } else if (adresse.value != previousValue) { // Si le contenu du champ de recherche a changé
+
+                previousValue = adresse.value;
+
+                if (previousRequest && previousRequest.readyState != XMLHttpRequest.DONE) {
+                    previousRequest.abort(); // Si on a toujours une requête en cours, on l'arrête
+                }
+
+                previousRequest = getResults(previousValue); // On stocke la nouvelle requête
+
+                selectedResult = -1; // On remet la sélection à "zéro" à chaque caractère écrit
+
+            }
+
+        });
+
+    })();
+    // ------------------------------------Appel API adresse data gouv lors du clic sur envoyer pour récupérer les coordonnées gps ----------------
+    $("#envoyer").click(function (e) {
+        e.preventDefault();
+        // On récupère l'adresse 
+        adressePostale = adresse.value;
+        $('#final').html('');
+        // On appelle la fonction getCoord    
+        getCoord();
+        return false;
+    });
+
+
+    // ------------------------------------Fonction localisation par adresse saisie---------------------------------- 
+    // ------------------------------------Et ajout du marqueur de postion sur la map et envoie de requête sur opendata doubs-------------------------------------------------------------
+
+    function getCoord() {
+        $.ajax({
+            type: 'GET',
+            url: apiAdresse,
+            timeout: 2000,
+            data: {
+                q: adressePostale
+            },
+            dataType: "json",
+            success: function (reponse) {
+                reponse = reponse.features; // On décompose la réponse pour trouver les éléments ciblés
+                reponse = reponse[0];
+                reponse = reponse.geometry;
+                reponse = reponse.coordinates;
+                lati = reponse[1]; // On récupère la lattitude
+                longi = reponse[0]; // On récupère la longitude
+                // On inverse lat et lng pour créer le marqueur
+                reponse = [
+                    lati,
+                    longi
+                ];
+                // On crée le marqueur sur la map
+                L.marker(reponse).addTo(mymap)
+                    .bindPopup("Vous êtes ici " + adressePostale + '<br>latitude : ' + lati + '<br>longitude : ' + longi).openPopup();
+                // On recentre la map
+                mymap.setView(
+                    reponse,
+                    15,
+                );
+            }
+        })
+    }
+
+})
